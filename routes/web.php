e2440c40726b0202b27a385c635012ea3633488a
Henry Lee <henryleeworld@gmail.com>
2023-05-23 00:03:58 +0800

    feat:升級至 laravel 9

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('stock/twse/show/', [TaiwanStockExchangeController::class, 'show']);
